49038440117cd4070c355815cc84b5cab2b5c841
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2023-02-01 18:51:07 +0530

    Add files via upload

---
+++ fibopss.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fib(int n){
+     if(n<=1)
+        return n;
+     return fib(n-1)+fib(n-2);
+}
+int main(){
+int num,n=0,i;
+printf("Enter the no of elements:");
+scanf("%d",&num);
+printf("\nfibonanci series:\n");
+for(i=1;i<=num;i++){
+    printf("%d\n",fib(n));
+    n++;
+}
+return 0;
+}
+
+
+
+
